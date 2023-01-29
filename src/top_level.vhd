ed30f08e96b49cb2162805337d0651d538189343
navodabandara <navoda.bandaralive@gmail.com>
2023-01-29 15:30:02 +0000

    Swapped std_logic_vector time for an integer

--- src/top_level.vhd
+++ src/top_level.vhd
@@ -12,7 +12,7 @@ entity counter is
 end counter;
 
 architecture behavioral of counter is
-    signal r_counter : std_logic_vector(7 downto 0) := "00000000"; --init counter to 0
+    signal decimal_counter  : integer := 0 ; --init counter to 0 (seems like this doesn't matter. Starts from the else in p_increment)
 
     --stuff related to clock driving
     constant c_clock_multiplier :  natural := 2700000; --clock frequency
@@ -26,7 +26,7 @@ begin
                 if r_clock_counter = c_clock_multiplier - 1 then --if the clock is about to overflow
                     r_clock_counter <= 0; --set the clock counter back to 0
                     --do stuff here
-                    r_counter <= r_counter + 1; --increment the other counter
+                    decimal_counter <= decimal_counter + 1; --increment the other counter
                 else
                     r_clock_counter <= r_clock_counter + 1; --else increment the clock counter by 1
                 end if;
@@ -34,11 +34,11 @@ begin
 
             if i_reset = '0' then --if the reset button is 0
                 r_clock_counter <= 0; --reset both counters
-                r_counter <= "00000000";
+                decimal_counter <= 0;
             end if;
 
     end process p_increment;
 
-    o_counter_drive <= r_counter; --set the counter driver as the value in counter
+    o_counter_drive <= conv_std_logic_vector(decimal_counter, o_counter_drive'length); --set the counter driver as the value in counter
 
 end behavioral;

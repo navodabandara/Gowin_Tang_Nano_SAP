327e0b2be9f757fa73792f96d70fb4a03e26cd10
navodabandara <navoda.bandaralive@gmail.com>
2023-02-04 21:34:48 +0000

    removed counter, added sysclk to top level

--- src/top_level.vhd
+++ src/top_level.vhd
@@ -4,29 +4,26 @@ use ieee.numeric_std.all;
 use IEEE.STD_LOGIC_ARITH.all;
 use ieee.std_logic_unsigned.all;
 
-entity counter is
+entity top_level is
     port(
         i_clock, i_reset : in std_logic;
-        o_counter_drive : out std_logic_vector(7 downto 0) := "00000000"
+        o_sysclk : out std_logic
     );
-end counter;
+end top_level;
 
-architecture behavioral of counter is
-    signal decimal_counter  : integer := 0 ; --init counter to 0 (seems like this doesn't matter. Starts from the else in p_increment)
-
+architecture behavioral of top_level is
     --stuff related to clock driving
     constant c_clock_multiplier :  natural := 2700000; --clock frequency
     signal r_clock_counter : natural range 0 to c_clock_multiplier; --max range is clock cycles per second
+    signal w_sysclk : std_logic := '0'; --system clock that the SAP 1 operates at
 
 begin
-
-    p_increment : process (i_clock, i_reset) is
+    sysclk_div : process (i_clock, i_reset) is
     begin
             if rising_edge (i_clock) then --on the rising edge of clock
                 if r_clock_counter = c_clock_multiplier - 1 then --if the clock is about to overflow
                     r_clock_counter <= 0; --set the clock counter back to 0
-                    --do stuff here
-                    decimal_counter <= decimal_counter + 1; --increment the other counter
+                    w_sysclk <= not w_sysclk; --toggle sysclk every c_clock_multiplier clock cycles
                 else
                     r_clock_counter <= r_clock_counter + 1; --else increment the clock counter by 1
                 end if;
@@ -34,11 +31,10 @@ begin
 
             if i_reset = '0' then --if the reset button is 0
                 r_clock_counter <= 0; --reset both counters
-                decimal_counter <= 0;
             end if;
+    end process sysclk_div;
 
-    end process p_increment;
+    o_sysclk <= not w_sysclk; -- show the sysclk on PIN10_IOL15A_LED1
 
-    o_counter_drive <= conv_std_logic_vector(decimal_counter, o_counter_drive'length); --set the counter driver as the value in counter
 
 end behavioral;
